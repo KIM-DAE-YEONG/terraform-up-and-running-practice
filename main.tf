f36fc534f769103aed70bc8b951616f30199e473
dy2023 <15bass@naver.com>
2023-01-01 23:31:09 +0900

    before asg

--- main.tf
+++ main.tf
@@ -3,10 +3,46 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-005835d578c62050d"
+  ami = "ami-02045ebddb047018b"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id, aws_security_group.forSSH.id]
 
+  user_data = <<-EOF
+		#!/bin/bash
+		echo "Hello, World" > index.html
+		nohup busybox httpd -f -p ${var.server_port} &
+		EOF
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "forSSH" {
+  name = "for ec2 connect"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
